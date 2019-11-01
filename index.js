c5063ac606c7c066af9f74147b7100a717417489
[traybournenorth] <[tpn26@nau.edu]>
2019-10-31 20:30:20 -0700

    Updating file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
 // Typescript Practice
 console.log("Hello world");
+console.log("This is where the converted Java will go to");
--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ import * as _ from 'lodash';
 
 // Typescript Practice
 console.log( "Hello world" );
+
+console.log( "This is where the converted Java will go to" );
