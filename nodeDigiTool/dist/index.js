db29208ae730f675bf0f6d8b5e0fdb5f1eebe2b3
Traybourne North <traybournenorth@Traybournes-MacBook-Pro.local>
2020-01-26 14:48:58 -0700

    Created new folder

--- nodeDigiTool/dist/index.js
+++ nodeDigiTool/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello world");
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//import * as React from "react";
+const ReactDOM = __importStar(require("react-dom"));
+ReactDOM.render(Hello, world < /h1>,, document.getElementById('root'));
--- nodeDigiTool/index.ts
+++ nodeDigiTool/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world");
+//import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>Hello world!</h1>,
+    document.getElementById('root')
+);
