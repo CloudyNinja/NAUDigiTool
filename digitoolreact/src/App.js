b5f3657272e4cdc5cfb57a11725d1d6f89f8db20
Traybourne North <traybournenorth@Traybournes-MacBook-Pro.local>
2020-01-26 15:23:08 -0700

    Minimized Files

--- digitoolreact/src/App.js
+++ digitoolreact/src/App.js
@@ -4,19 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+      <div class="login-box">
+      <h1>Login</h1>
+      <div class="textbox">
+        <form name="login">
+        <div class="login">
+          <input type="text" placeholder="Username" name="userid"></input>
+          <input type="password" placeholder="Password" name="pswrd"></input>
+          <input type="button" onclick="checkForm" target="_self" value="Login"></input>
+          <input type="button" onclick="register() "Value="Sign Up"></input>
+
+        </div>
+        </form>
+        <code name="checkForm">
+          if( form.userid.value == "" || form.pswrd.value == "" )
+            {
+              alert( "You must fill in both fields to log in." )
+            }
+            else 
+            {
+              /*window.open( 'https://dana.ucc.nau.edu/tpn26/NAUDigiTool/homepage.html' )*/
+              window.location.href = "homepage.html"
+            }
+          }
+          
+        function register() {
+          window.location.href = "register.html"
+        }</code>
+      </div>
+    </div>
     </div>
   );
 }
