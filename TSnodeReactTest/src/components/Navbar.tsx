5be621db7998fc41d75ecdc765417048216e2c73
Traybourne North <traybournenorth@Traybournes-MacBook-Pro.local>
2020-01-26 15:55:07 -0700

    TS Folder

---
+++ TSnodeReactTest/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+
+export class Navbar extends React.Component< {}> {
+    render() {
+        return <h1>Hello from  and !</h1>;
+    }
+}
---
+++ TSnodeReactTest/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Navbar } from "./components/Navbar";
+
+ReactDOM.render(
+    <Navbar />,
+    document.getElementById("example")
+);
