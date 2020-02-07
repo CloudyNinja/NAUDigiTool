9b1b8f8265e9a1f7170699c07258e163dc141384
Traybourne North <traybournenorth@Traybournes-MacBook-Pro.local>
2020-02-07 14:58:11 -0700

    Update moduleOneQ3.js

--- NewPrototype/JavaScriptFiles/Module_1/moduleOneQ3.js
+++ NewPrototype/JavaScriptFiles/Module_1/moduleOneQ3.js
@@ -6,14 +6,16 @@ createTruthTable(3);
 createKMap(3);
 fillKMap();
 
-/*array[0] = 0;
+/*
+array[0] = 0;
 array[1] = 0;
 array[2] = 0;
 array[3] = 0;
 array[4] = 0;
 array[5] = 0;
 array[6] = 0;
-array[7] = 0;*/
+array[7] = 0;
+*/
 
 printArray();
 findOctetGroups();
