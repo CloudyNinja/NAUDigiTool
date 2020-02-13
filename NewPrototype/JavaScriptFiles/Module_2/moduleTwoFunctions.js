ad34ae8de19690ebf31c3c71da9fd9201cf7a0dd
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2020-02-13 15:16:36 -0700

    Renamed ConversionTest to moduleTwoQuestionOne

--- NewPrototype/JavaScriptFiles/Module_2/moduleTwoFunctions.js
+++ NewPrototype/JavaScriptFiles/Module_2/moduleTwoFunctions.js
@@ -1,8 +1,9 @@
 
-// Global variable
+// Global variables
 var randomNum = Math.floor( ( Math.random() * 1000 ) + 1 ) 
 var randomNum2 = Math.floor( ( Math.random() * 1000 ) + 1 )
 var randomNum3 = Math.floor( ( Math.random() * 1000 ) + 1 )
+var randomNum4 = Math.floor( ( Math.random() * 1000 ) + 1 )
 
 // Generates random decimal number
 function random()
@@ -10,6 +11,7 @@ function random()
     document.getElementById("randomNumber1").innerHTML = randomNum
     document.getElementById("randomNumber2").innerHTML = randomNum2
     document.getElementById("randomNumber3").innerHTML = randomNum3
+    document.getElementById("randomNumber4").innerHTML = randomNum4
 }
 
 // Checks if hex value is correct
@@ -40,6 +42,17 @@ function submitHex()
     var answer3 = randomNum3.toString(16)
     var userInput3 = document.getElementById("hex3").value
     if( userInput3.toUpperCase() == answer3.toUpperCase() )
+    {
+        alert("Congratulations, correct answer!")
+    }
+    else
+    {
+    alert("Incorrect, try again.")
+    }
+
+    var answer4 = randomNum4.toString(16)
+    var userInput4 = document.getElementById("hex4").value
+    if( userInput4.toUpperCase() == answer4.toUpperCase() )
     {
         alert("Congratulations, correct answer!")
     }
