8e08afaaf09c472f0c2eb42aa85ef0e0f4ce0468
Spencer Clark <qxspencerxp@yahoo.com>
2020-03-25 12:28:17 -0700

    Remove debug lines

--- NewPrototype/JavaScriptFiles/tracker.js
+++ NewPrototype/JavaScriptFiles/tracker.js
@@ -5,20 +5,10 @@
 function db_log( id, module_num, mode, success, stars, mistakes, time_taken) {
     var xmlhttp = new XMLHttpRequest();
     //open is the method, file on the serverside to access, and async or not
-
-    alert(""+id + module_num+ mode+ success+ stars+ mistakes+ time_taken);
-
-    xmlhttp.open("POST", "../../tracker.php", false);
+    xmlhttp.open("POST", "../../tracker.php", true);
 
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            let response_data = xmlhttp.responseText
-            //string instead of bool since converting is unecessary
-           alert(response_data);
-        }
-    }
     //send is the variables passed through POST that the php file can use
     xmlhttp.send("id="+id+"&mistakes="+mistakes+"&module_num="+module_num+
                 "&success="+success+"&time="+time_taken+"&mode="+mode+"&stars="+stars);
